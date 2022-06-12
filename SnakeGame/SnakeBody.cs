2d2de158d518d224c8f15398bbe371e1f2cf216b
wkfyd <106089929+wkfyd@users.noreply.github.com>
2022-06-13 05:15:14 +0900

    UpLoad

---
+++ SnakeGame/SnakeBody.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    class SnakeBody
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public SnakeBody()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
